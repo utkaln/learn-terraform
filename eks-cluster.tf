189835f61067f247686ddf50c2dafd49e567e3ad
Utkal Nayak <nayak.utkal@gmail.com>
2022-02-01 01:11:29 -0500

    added kuberenetes provider to link to fargate profile

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,18 @@
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.jan22-eks-cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.jan22-eks-cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.jan22-eks-cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "jan22-eks-cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "jan22-eks-cluster" {
+  name = module.eks.cluster_id
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "18.2.3"
@@ -16,7 +31,7 @@ module "eks" {
 
   fargate_profiles = {
     default = {
-      name = "default"
+      name = "fargate"
       selectors = [
         {
           namespace = "kube-system"
@@ -25,7 +40,7 @@ module "eks" {
           }
         },
         {
-          namespace = "default"
+          namespace = "fargate"
         }
       ]
 
